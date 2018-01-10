a27d85f6a85c0212f47cc776cf7ef5ad6fd9deeb
kms1837 <kms1837@gmail.com>
2018-01-11 03:29:21 +0900

    added draw line support, combo system

--- Classes/scene/test_scene.cpp
+++ Classes/scene/test_scene.cpp
@@ -8,12 +8,14 @@ USING_NS_CC;
 TestScene::TestScene():
 score_(0),
 combo_(0),
-gameTimer_(0) {
+gameTimer_(0),
+dengerF_(false),
+prevTouchPoint_(Vec2(-1, -1)) {
 
 }
 
 TestScene::~TestScene() {
-
+	
 }
 
 Scene* TestScene::scene() {
@@ -31,23 +33,24 @@ bool TestScene::init() {
 	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("sound/SHORT_BGM_09.mp3");
 	circles_ = CircleManager::create(this);
 
-	int margin = 30;
-
 	//Label Object
 	TTFConfig ttfConfig(kLabelFont, kLabelFontSIze);
 	auto scoreTitle = Label::createWithTTF(ttfConfig, "score: ", TextHAlignment::LEFT, winSize.width);
-	scoreTitle->setPosition(kLabelFontSIze + margin, winSize.height - 30);
+	scoreTitle->setPosition(kLabelFontSIze + kUImargin, winSize.height - 30);
 	scoreTitle->setName("scoreLabel");
 	this->addChild(scoreTitle, kLabelDepth);
 
 	scoreLabel_ = Label::createWithTTF(ttfConfig, "0", TextHAlignment::RIGHT, 150);
 	// issue - score Label size
-	scoreLabel_->setPosition(300, winSize.height - margin);
+	scoreUIPosition_ = Vec2(300, winSize.height - kUImargin);
+	scoreLabel_->setPosition(scoreUIPosition_);
 	scoreLabel_->setName("scoreLabel");
 	this->addChild(scoreLabel_, kLabelDepth);
 
-	circleCountLabel_ = Label::createWithTTF(ttfConfig, "0", TextHAlignment::CENTER, winSize.width);
-	circleCountLabel_->setPosition(winSize.width / 2, winSize.height - margin);
+	string circleCount = to_string(kStartCircleCount);
+
+	circleCountLabel_ = Label::createWithTTF(ttfConfig, circleCount, TextHAlignment::CENTER, winSize.width);
+	circleCountLabel_->setPosition(winSize.width / 2, winSize.height - kUImargin);
 	circleCountLabel_->setName("circleCountLabel");
 	this->addChild(circleCountLabel_, kLabelDepth);
 
@@ -65,10 +68,10 @@ bool TestScene::init() {
 													  CC_CALLBACK_0(TestScene::pause, this));
 
 	Menu *stopmenu = Menu::create(stopButton, nullptr);
-	stopmenu->setPosition(winSize.width - margin, winSize.height - margin);
+	stopmenu->setPosition(winSize.width - kUImargin, winSize.height - kUImargin);
 	addChild(stopmenu);
 
-	for (int i = 0; i < 12; i++) {
+	for (int i = 0; i < kStartCircleCount; i++) {
 		circles_->createCircle();
 	}
 
@@ -76,6 +79,12 @@ bool TestScene::init() {
 	this->schedule(schedule_selector(TestScene::gameRunCounter), 1.0);
 
 	return true;
+}
+
+void TestScene::gameRun() {
+	this->schedule(schedule_selector(TestScene::gameTimerRunning));
+	this->schedule(schedule_selector(TestScene::circleCreating), kCreateInterval);
+	this->scheduleUpdate();
 }
 
 void TestScene::update(float dt) {
@@ -113,12 +122,16 @@ void TestScene::gameRunCounter(float dt) {
 		_eventDispatcher->addEventListenerWithFixedPriority(listener, 1);
 
 		this->removeChildByName("counterLabel");
-		this->schedule(schedule_selector(TestScene::gameTimerRunning));
+		gameRun();
 		this->unschedule(schedule_selector(TestScene::gameRunCounter));
-		this->scheduleUpdate();
 	}
 
 	gameTimer_--;
+}
+
+void TestScene::comboTimer(float dt) {
+	this->unschedule(schedule_selector(TestScene::comboTimer));
+	combo_ = 0;
 }
 
 void TestScene::circleInnerVisual(float dt) {
@@ -130,17 +143,69 @@ void TestScene::circleInnerVisual(float dt) {
 	}
 }
 
+void TestScene::circleCreating(float dt) {
+	int circleCount = 0;
+
+	for (int i = 0; i < kBoomCount; i++) {
+		circles_->createCircle();
+	}
+
+	circleCount = circles_->getSize();
+
+	if (circleCount >= kMaxiumCircleCount) {
+		gameOver();
+	}
+
+	if (circleCount >= kMaxiumCircleCount - kStartCircleCount && !dengerF_) {
+		dengerF_ = true;
+		denger();
+	} else if (circleCount < kMaxiumCircleCount - kStartCircleCount && dengerF_) {
+		dengerF_ = false;
+		//this->removeChildByTag(GAMEDENGERLAYERTAG);
+	}
+
+	string circleCountStr = to_string(circleCount);
+	circleCountLabel_->setString(circleCountStr);
+}
+
 void TestScene::circleBoom() {
 	auto innerCircle = circles_->getInnerCircles(touchMovePoints_);
 
-	if (innerCircle.size() == 3) {
+	if (innerCircle.size() == kBoomCount) {
+		int comboScore;
+		Size winSize = Director::getInstance()->getWinSize();
 		for (auto circle : innerCircle) {
-			circles_->remove(circle);
+			plusScoreAction(circle->sprite->getPosition(), kDefaultPlusScore);
+			circles_->removeCircle(circle);
+			score_ += kDefaultPlusScore;
 		}
 
-		string circleCount = to_string(circles_->getCircles().size());
+		combo_++;
+
+		comboScore = (combo_ * kComboScore);
+
+		score_ += comboScore;
+		string scoreStr = to_string(score_);
+		scoreLabel_->setString(scoreStr);
+
+		if (circles_->getSize() <= 0) {
+			for (int i = 0; i < kStartCircleCount; i++) {
+				circles_->createCircle();
+			}
+		}
+
+		string circleCount = to_string(circles_->getSize());
 		circleCountLabel_->setString(circleCount);
+
+		plusScoreAction(Vec2(winSize.width/2, winSize.height/2), comboScore);
+
+		this->unschedule(schedule_selector(TestScene::comboTimer));
+		this->scheduleOnce(schedule_selector(TestScene::comboTimer), kComboDuration);
 	}
+}
+
+void TestScene::denger() {
+
 }
 
 void TestScene::pause() {
@@ -148,7 +213,26 @@ void TestScene::pause() {
 }
 
 void TestScene::gameOver() {
+	this->unscheduleAllSelectors();
+	//circles_->remove();
+}
 
+void TestScene::plusScoreAction(Vec2 startPoint, int plusScore) {
+	TTFConfig ttfConfig(kLabelFont, kLabelFontSIze);
+	string plusScoreStr = to_string(plusScore);
+	Label* comboScoreLabel = Label::createWithTTF(ttfConfig, plusScoreStr, TextHAlignment::CENTER, 100);
+	comboScoreLabel->setPosition(startPoint);
+	this->addChild(comboScoreLabel);
+
+	auto removeLabel = CallFunc::create([this, comboScoreLabel]() {
+		this->removeChild(comboScoreLabel);
+	});
+
+	auto sequence = Sequence::create(FadeOut::create(0.5f),
+									 removeLabel, nullptr);
+
+	comboScoreLabel->runAction(MoveTo::create(0.5f, scoreUIPosition_));
+	comboScoreLabel->runAction(sequence);
 }
 
 bool TestScene::onTouchesBegan(Touch* touch, Event* event) {
@@ -159,6 +243,14 @@ bool TestScene::onTouchesBegan(Touch* touch, Event* event) {
 	touchStartPoint->setPosition(touchPosition);
 	touchStartPoint->setName("startPoint");
 	this->addChild(touchStartPoint);
+
+	auto touchLine = DrawNode::create();
+	touchLine->setName("drawLine");
+	addChild(touchLine);
+
+	auto supportLine = DrawNode::create();
+	supportLine->setName("supportLine");
+	addChild(supportLine);
 
 	MotionStreak* streak = MotionStreak::create(0.5, 3, 50, Color3B::WHITE, "paddle.png");
 	streak->setPosition(touchPosition);
@@ -175,6 +267,18 @@ void TestScene::onTouchesMoved(Touch* touch, Event* event) {
 
 	MotionStreak* streak = (MotionStreak*)this->getChildByTag(123);
 
+	auto touchLine = (DrawNode*)this->getChildByName("drawLine");
+	auto supportLine = (DrawNode*)this->getChildByName("supportLine");
+	auto startPoint = this->getChildByName("startPoint");
+
+	if (prevTouchPoint_.x != -1) {
+		supportLine->clear();
+		supportLine->drawLine(startPoint->getPosition(), touchPosition, Color4F::BLUE);
+		touchLine->drawLine(prevTouchPoint_, touchPosition, Color4F::BLUE);
+	}
+
+	prevTouchPoint_ = touchPosition;
+
 	touchMovePoints_.push_back(touchPosition);
 
 	streak->setPosition(touchPosition);
@@ -184,6 +288,9 @@ void TestScene::onTouchesMoved(Touch* touch, Event* event) {
 void TestScene::onTouchesEnded(Touch* touch, Event* event) {
 	this->removeChildByTag(123);
 	this->removeChildByName("startPoint");
+	this->removeChildByName("drawLine");
+	this->removeChildByName("supportLine");
+
 	touchMovePointSpr_->setPosition(-10, -10);
 
 	circleBoom();
@@ -191,6 +298,8 @@ void TestScene::onTouchesEnded(Touch* touch, Event* event) {
 	touchMovePoints_.clear();
 
 	circles_->styleClear();
+
+	prevTouchPoint_ = Vec2(-1, -1);
 
 	this->unschedule(schedule_selector(TestScene::circleInnerVisual));
 }
--- Classes/scene/test_scene.h
+++ Classes/scene/test_scene.h
@@ -26,17 +26,35 @@ private:
 	const int kLabelFontSIze = 40;
 	const int kLabelDepth = 1;
 	const int kMaxiumCircleCount = 50; // 게임 종료 조건
+	const int kStartCircleCount = 12; // 시작 구체 개수
+	const int kBoomCount = 3; // 맞춰야 하는 구체 개수
+	const int kDefaultPlusScore = 100; // 기본 추가 점수
+	const int kComboScore = 10; // 콤보 가중 점수
+	const int kUImargin = 40;
+	const float kCreateInterval = 3.0f; // 구체등장 시간
+	const float kComboDuration = 3.0f; // 콤보유지 시간
 
-	//Scheduler
+	// UI
+	cocos2d::Vec2 scoreUIPosition_;
+
+	// flag
+	bool dengerF_;
+
+	// Scheduler
 	void update(float dt);
 	void gameTimerRunning(float dt);
 	void gameRunCounter(float dt);
 	void comboTimer(float dt);
 	void circleInnerVisual(float dt);
+	void circleCreating(float dt);
 
+	void gameRun();
 	void circleBoom();
 	void pause();
 	void gameOver();
+	void denger();
+
+	void plusScoreAction(cocos2d::Vec2 startPoint, int plusScore);
 
 	CircleManager* circles_;
 
@@ -47,6 +65,8 @@ private:
 	cocos2d::Label* scoreLabel_;
 	cocos2d::Label* circleCountLabel_;
 	cocos2d::Sprite* touchMovePointSpr_;
+
+	cocos2d::Vec2 prevTouchPoint_;
 
 	std::list<cocos2d::Vec2> touchMovePoints_;
 
